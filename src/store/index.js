dd8d748c0ff75279f1d107f266e8b4563a02be7a
Vivek Sudani <viveksudani7@gmail.com>
2022-04-22 14:55:38 +0530

    updated coaches components

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
-import {createStore} from 'vuex';
+import { createStore } from 'vuex';
 
 import coachesModules from './modules/coaches/index.js';
 
 const store = createStore({
-    modules: {
-        coaches: coachesModules
-    },
-})
-
+  modules: {
+    coaches: coachesModules,
+  },
+  state() {
+    return {
+      userId: 'c3',
+    };
+  },
+  getters: {
+    userId(state){
+        state.userId
+    }
+  }
+});
 
 export default store;
