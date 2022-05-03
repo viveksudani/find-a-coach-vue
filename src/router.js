21174f713fe70d6783f435db518b242a6f807bdd
Vivek Sudani <viveksudani7@gmail.com>
2022-05-03 17:43:41 +0530

    fixed router guard problem

--- src/router.js
+++ src/router.js
@@ -40,7 +40,7 @@ const router = createRouter({
 router.beforeEach(function (to, _, next) {
   if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
     next('/auth');
-  } else if (to.meta.requiresAuth && store.getters.isAuthenticated) {
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
     next('/coaches');
   } else {
     next();
