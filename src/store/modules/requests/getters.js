b2649eab6507eab86c12559d377b5a2b096b1a09
Vivek Sudani <viveksudani7@gmail.com>
2022-05-03 17:42:53 +0530

    fixed data update bug

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -6,12 +6,4 @@ export default {
   hasRequests(_, getters) {
     return getters.requests && getters.requests.length > 0;
   },
-  shouldUpdate(state) {
-    const lastFetch = state.lastFetch;
-    if (!lastFetch) {
-      return true;
-    }
-    const currentTimeStamp = new Date().getTime();
-    return (currentTimeStamp - lastFetch) / 1000 > 60;
-  },
 };
